66461eb1fc092480eb47dd4d72b73ecd482fdce5
Pik2Pika <ja2002t@gmx.com>
2022-09-06 11:42:31 -0400

    complete challenge

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    //if (string == '') return string;
+    stringArray = string.split('');
+    stringArray.reverse();
+    revString = stringArray.join('');
+    return revString;
 };
 
 // Do not edit below this line
