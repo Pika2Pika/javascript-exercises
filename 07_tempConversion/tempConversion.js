a2307d3555aaff6255c0922560512a36a43dd5c8
Pik2Pika <ja2002t@gmx.com>
2022-09-06 15:16:57 -0400

    challenge complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (f - 32) * (5/9);
+  c = c.toFixed(1); //toFixed returns a string rounded to one decimal place 
+  return +c; //converts string to num 
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+// 
+  let f = c * (9/5) + 32; 
+  f = f.toFixed(1);
+  return +f;
 };
-
+//round to one decimal 
 // Do not edit below this line
 module.exports = {
   ftoc,
