a7b55a5b78e3d3b36fb79619949fb12ad271b655
Pik2Pika <ja2002t@gmx.com>
2022-09-06 12:17:21 -0400

    complete challenge

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(givenArray, ...delValues) {
+    delValues = Array.from(delValues);
+    //iterate through del values grab index of in given array splice given array and remove one item 
+    for(let i = 0; i < delValues.length; i++){
+        let index = givenArray.indexOf(delValues[i]); 
+        if (index >= 0)  givenArray.splice(index,1);
+    }
+    return givenArray;
 };
 
 // Do not edit below this line
