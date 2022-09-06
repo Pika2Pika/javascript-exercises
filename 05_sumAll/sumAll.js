0870c1e9a802621f300fa81b592d0de4f9a534c8
Pik2Pika <ja2002t@gmx.com>
2022-09-06 12:45:08 -0400

    challenge complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if (a < 0 || b < 0) return 'ERROR';
+    //if (Number.isNaN(a) || Number.isNaN(b)) return 'ERROR';
+    if (typeof(a) !== 'number' || typeof(b) !== 'number') return 'ERROR';
 
+    let largest = a > b ? a : b;
+    let smallest = a > b ? b : a;
+    let sum = 0;
+
+    for (let i = smallest; i <= largest ; i++) {sum += i;}
+    return sum;
 };
-
+//error with negs- reminds me of factorials looping through and using that i value- the largest value can be first so maybe add a check for the largest number and use that as the conditional for iterVar 
 // Do not edit below this line
 module.exports = sumAll;
